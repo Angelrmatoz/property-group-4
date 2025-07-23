203ac1062d3997c3c2a9263f2ca187863a1a8660
Angelrmatoz <angelrmatoz@gmail.com>
2025-07-23 01:17:26 -0400

    Refactor code structure for improved readability and maintainability

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://172.16.0.2:3000"
+  ],
   eslint: {
     ignoreDuringBuilds: true,
   },
