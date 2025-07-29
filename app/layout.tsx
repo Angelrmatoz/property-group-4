4385b608b410ea762d63e5447658d93c3b73395b
Angelrmatoz <angelrmatoz@gmail.com>
2025-07-29 16:16:56 -0400

    fix: update layout component to use Head from next/head for improved SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Property Group",
@@ -13,9 +14,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <link rel="icon" href="./images/icons/PG-icon-dorado.png" />
-      </head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1090379115079834');
+fbq('track', 'PageView');`,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1090379115079834&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </Head>
       <body>{children}</body>
     </html>
   );
