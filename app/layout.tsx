1692bbb6c6e2fa3170b120d4334e35d6f695fd2c
Angelrmatoz <angelrmatoz@gmail.com>
2025-08-13 01:03:30 -0400

    fix: reorder Meta Pixel code and restore noscript tag in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import "../styles/globals.css";
 
 export const metadata: Metadata = {
@@ -16,6 +15,21 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="./images/icons/PG-icon-dorado.png" />
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1063226029330522');
+fbq('track', 'PageView');`,
+          }}
+        />
         <noscript>
           <img
             height="1"
@@ -25,24 +39,9 @@ export default function RootLayout({
             alt="Meta Pixel"
           />
         </noscript>
-      </head>
-      <body>
-        {/* Meta Pixel Code */}
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1063226029330522');
-fbq('track', 'PageView');`}
-        </Script>
         {/* End Meta Pixel Code */}
-        {children}
-      </body>
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
